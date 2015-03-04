f58f4fa8f26388362e8edc01755c4a7050699cac
bdwalker1 <bdwalker@ureach.com>
2015-03-03 16:49:38 -0800

    First Rev

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblMessage: UILabel!
+    @IBOutlet weak var txtName: UITextField!
+    @IBOutlet weak var txtMsg: UITextField!
+    @IBOutlet weak var btnSendMail: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func beginEditTxtName(sender: AnyObject) {
+        txtName.becomeFirstResponder()
+
+        btnSendMail.setTitle("Send Mail", forState: UIControlState.Normal )
+    }
+    
+    @IBAction func pressBtnSendMail(sender: UIButton) {
+        lblMessage.textColor = UIColor.greenColor()
+        lblMessage.text = txtName.text + ",\r\n\t" + txtMsg.text
+        lblMessage.hidden = false
+        txtName.text = ""
+        txtMsg.text = ""
+        txtMsg.resignFirstResponder();
+
+        btnSendMail.setTitle("Mail Sent", forState: UIControlState.Normal )
+    }
 
 }
 
